2ab960b7ef9afda4db3a33a0dcf52a7d1253ff89
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-04-20 01:56:21 -0400

    feat: got initial model working and outputting to the terminal

--- model.py
+++ model.py
@@ -4,6 +4,140 @@ model.py: Define your model class inheriting from sklearn's RandomForestRegresso
 Includes:
     - constructor
     - train function
+    - decay function (will be multiplied with the predicted score to obtain the final score)
     - predict_score function (with decay functionality)
     - get_difficulty function
 """
+
+import math
+import time
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+class DifficultyModel():
+    def __init__(self):
+        self.model = RandomForestRegressor(n_estimators=100, max_depth=10)
+        self.training_done = False
+
+    def train(self, df):
+        """
+        Train the RandomForestRegressor model using the provided training data.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame 
+            A DataFrame containing the features [correctLast3, avgTime, confidence, quizScore, sessions].
+        """
+        X = df.drop(columns=['timeSpent', 'readinessScore', 'numberQuestionsAnswered'])
+        Y = df['readinessScore']
+
+        self.model.fit(X, Y)
+        self.training_done = True
+
+    def decay_function(self, days_since_last_login):
+        """
+        A decay function.
+
+        This function applies a decay to the output of the model.predict_score() function
+        according to the number of days since the user last logged in. A decay score ranges between
+        [0.10, 1.00]. A larger output closer to 1.00 implies no decay and a smaller output implies 
+        greater decay.
+
+        Parameters
+        ----------
+        days_since_last_login : int
+            The number of days since a user has last logged in
+        """
+        try:
+            if days_since_last_login < 0:
+                raise ValueError('Value is not zero or a positive integer.')
+            
+            if days_since_last_login >= 30:
+                return 0.10
+            elif days_since_last_login >= 3:
+                return 1 - math.log(days_since_last_login, 10) + 0.477
+            else:
+                return 1.00
+        except ValueError as e:
+            print(f'Invalid input: {e}')
+
+    def predict_score(self, input, last_logged_in):
+        """
+        A prediction function.
+
+        This function calls both the decay function to grab the rate of decay and the predicted score
+        from the model based on user provided input to output a final score that signifies how ready a
+        user is for the next jump in question difficulty. The decay rate is multipled by the model's
+        predicted score from the input.
+
+        Parameters
+        ----------
+        input : pandas.DataFrame
+            Expects 5 columns that signify values of 
+            [correctLast3, avgTime, confidence, quizScore, sessions] where each column has data values for
+            [number of last 3 question that were correct, average time on each question in seconds,
+            confidence value between 0.00-1.00, quiz score of the last quiz between 0.00-1.00, total
+            number of sessions taken].
+        """
+        decay_rate = self.decay_function(last_logged_in)
+        model_score = self.model.predict(input)
+        
+        print(f'last logged in: {last_logged_in} | decay rate: {decay_rate} | model score: {model_score}', end='')
+
+        return model_score * decay_rate
+
+    def get_difficulty(self, predicted_score):
+        """
+        A difficulty function.
+
+        This function checks the predicted score and returns whether the question will be easy, medium,
+        or hard for that specific classroom subject and grade level.
+
+        Parameters
+        ----------
+        predicted_score : float
+            A score of how prepared the user is to take on the next question.
+        """
+        try:
+            if predicted_score < 0 or predicted_score > 1:
+                raise ValueError("Predicted score is outside normalization range, [0,1].")
+            
+            if predicted_score < 0.30:
+                return 'easy'
+            elif predicted_score >= 0.30 and predicted_score < 0.65:
+                return 'medium'
+            else:
+                return 'hard'
+        except ValueError as e:
+            print(f'Invalid predicted score: {e}')
+
+    def listen_for_training(self, columns, inputs):
+        """
+        A listener function.
+
+        This function listens first for the result of the model being trained. Once done with training,
+        the function will predict a score for every row of inputs. The result will be shown in the 
+        terminal. More relevant for larger datasets. 
+
+        Parameters
+        ----------
+        columns : list[str]
+            In this order, expects 
+            ['correctLast3', 'avgTime', 'confidence', 'quizScore', 'sessions', 'lastLoggedIn'].
+        
+        inptus : list[list[float]]
+            Each row in the 2-D Array contains 6 columns following the columns data values.
+            The last column contains the number of days since last login, which is expected to come
+            from some type of database that gets updated every time a user logs into the application.
+        """
+        while not self.training_done:
+            print('Model still training...')
+            time.sleep(1)
+        
+        for input in inputs:
+            last_logged_in = input[-1]
+            x_input = pd.DataFrame([input], columns=columns).drop(columns=['lastLoggedIn'])
+
+            predicted_score = self.predict_score(x_input, last_logged_in)
+
+            print(f' | predicted score: {predicted_score} | input: {input}')
