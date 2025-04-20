fea0906d79fec0cf720bc9f92d05be93f4de4cb4
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-04-20 01:05:00 -0400

    feat: successfully pulling from CSV file

--- data.py
+++ data.py
@@ -4,3 +4,8 @@ data.py: Function to load training data on student statistics for taking quizzes
 Function:
     - load_student_data(): Reads a CSV file into a DataFrame and returns it.
 """
+
+import pandas as pd
+
+def load_student_data():
+    return pd.read_csv('dataset/student_statistics.csv')
