d0898a6c9407ec3b1cf6ccfb6a77454d1bac0cdc
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-04-25 21:22:32 -0400

    refactor: updated pseudocode for regression tree implementation

--- random_forest_implementation/regression_tree.py
+++ random_forest_implementation/regression_tree.py
@@ -1,19 +1,39 @@
 from tree_node import TreeNode
+import pandas as pd
 
 def createRegressionTree(dataset):
-    # Start with the first column variable, Age
-        # Try different threshold values of Age and calculate the sum of squared residual for the current threshold value.
-            # Each threshold value will be found by looping over the rows, for every 2 rows grab the Ages and compute average Age.
-            # Find the average age values for all observations less than the average of the 2 chosen points and greater than. These will be left and right leaves.
-            # For both left & right leaves, calculate each observations residual by taking actual Age - predicted Age (average). Then square them.
-            # Sum of squared residuals found by adding up all your calculated squared residuals. You can plot these on a graph where x-axis = variable threshold and y-axis = sum of squared residuls
-                # to visually views which threshold value has the smallest sum of squared residuals.
-            # Keep track of the threshold value with the smallest sum of squared residual, resets after you move to next column.
-        # After best threshold value is found for Age, create the root node with the rule "Age < {threshold_value}"
-        # Find the number of observations(rows) where Age is less than and greater than threshold
-        # If number of observations less than threshold of Age is < N (tbd), stop splitting and create a left leaf node with the prediction being the average target variable of all remaining observations.
-        # Else, continue splitting or subdividing the observation of Age
-        # If number of observations greater than threshold of Age is < N (tbd), stop splitting and create a left leaf node with the prediction being the average target variable of all remaining observations.
-    # Do the same for the rest of the columns, adding to the regression tree
+    # Initialize a variable to store the column and its sum of squared residuals value that is the lowest of all columns
+    # Initialize min_samples to stop splitting 
+
+    # For each column in your dataset that influences the target variable (e.g. Age, Sex, Dosage):
+        # Initialize a variable to hold the best threshold value for that column, best_threshold, to None
+        # Initialize a variable to hold the sum of squared residuals that is the minimum of all found, to positive infinity
+        # For each row in your dataset specific to that column up to the second to last row:
+            # Initialize a variable to hold the current sum of squared residuals, to postivie infinity
+            # Grab the current row and next row
+            # Calculate the current threshold by taking the average of both rows, store in current_threshold
+            # Find the average target variable less than the threshold by summing up all the datapoints target variable / number of datapoints (left)
+            # Find the average target variable greater than the threshold by summing up all the datapoints target variable / number of datapoints (right)
+            # For all datapoints less than the current_theshold:
+                # Compute a squared residual for that datapoint | squared residual = (actual target variable - left_average)^2
+                # Add it to the current sum of squared residuals variable
+            # For all datapoints greater than the average of both rows:
+                # Compute a squared residual for that datapoint | squared residual = (actual target variable - right_average)^2
+                # Add it to the current sum of squared residuals variable
+        # Compare the sum of squared residuals with the current best. If less than, replace. Else, keep the same.
     
+    # Now you have the best column. Make it the root node, passing in the appropriate rule in the node creation
+    # Remove that column from the dataset
+    # If number of observations less than the threshold is less than min_samples
+        # Calculate average prediction
+        # Create leaf node with the average prediction
+    # Else:
+        # Continue splitting with the updated dataset (Recursive Partitioning)
+
+    # If number of observations greater than the threshold is less than min_samples
+        # Calculate average prediction
+        # Create leaf node with the average prediction
+    # Else:
+        # Continue splitting with the updated dataset. (Recursive Partitioning)
+
     return ""
