fa8402231c047016ea94dc21bd0f09a884d5b350
Ali <ah4100574@gmail.com>
2025-04-27 22:20:15 -0400

    refactor: changed main file to Main.java from HelloWorld.java.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,15 @@
 package com.example;
 import smile.data.DataFrame;
 import smile.io.Read;
+import com.example.RandomForestRegressor;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         try {
-            DataFrame df = Read.csv("src/main/resources/dataset/student_statistics.csv");
-            System.out.println("First 5 rows of Data:");
-            System.out.println(df.head(5));
+            DataFrame df = Read.csv("src/main/resources/dataset/student_statistics.csv", "header=true");
+            RandomForestRegressor model = new RandomForestRegressor(5, 5, 10);
+            model.bootstrapSample(df);
+
         } catch (java.io.IOException | java.net.URISyntaxException e) {
             System.err.println("Failed to read CSV file: " + e.getMessage());
         }
